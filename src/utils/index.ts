d464ae4351b9d80b642d668db6a2f15982d86f00
小K <904656778@qq.com>
2023-07-10 16:34:33 +0800

    优化代码

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -23,33 +23,31 @@ function formatType(type: string) {
 
 export function flatObjects(data: any) {
   const result: any = {};
-  if (data?.properties) {
-    const { properties } = data;
-    for (const key in properties) {
-      const item = properties[key];
-      if (item.type === "object") {
-        result[key] = {
-          type: formatType(item.type),
-          description: item?.description,
-          object: flatObjects(item),
-        };
-      } else if (item.type === "array" && !item?.items.properties) {
-        result[key] = {
-          type: `${formatType(item?.items.type)}[]`,
-          description: item?.description,
-        };
-      } else if (item.type === "array") {
-        result[key] = {
-          type: formatType(item.type),
-          description: item?.description,
-          object: flatObjects(item.items),
-        };
-      } else {
-        result[key] = {
-          type: formatType(item.type),
-          description: item?.description,
-        };
-      }
+  const { properties } = data || {};
+  for (const key in properties || {}) {
+    const item = properties[key];
+    if (item.type === "object") {
+      result[key] = {
+        type: formatType(item.type),
+        description: item?.description,
+        object: flatObjects(item),
+      };
+    } else if (item.type === "array" && !item?.items?.properties) {
+      result[key] = {
+        type: `${formatType(item?.items?.type)}[]`,
+        description: item?.description,
+      };
+    } else if (item.type === "array") {
+      result[key] = {
+        type: formatType(item.type),
+        description: item?.description,
+        object: flatObjects(item.items),
+      };
+    } else {
+      result[key] = {
+        type: formatType(item.type),
+        description: item?.description,
+      };
     }
   }
   return result;
@@ -58,34 +56,29 @@ export function flatObjects(data: any) {
 /** transform to typescript interface */
 export function toTypescript(data: any, options?: { key?: string; value?: string }) {
   let result = "";
-  for (const key in data) {
+  for (const key in data || {}) {
     const item = data[key];
     const annotation = `/**
           * ${item?.description || "TODO: unknow"}
           * */`;
 
-    if (options && options?.key?.trim() === key?.trim()) {
+    if (options?.key?.trim() === key?.trim()) {
       result += `
         ${annotation}
-        ${key}: ${options?.value};
+        ${key}: ${options.value};
         `;
     } else {
       console.log("%c输出:", "color: #007acc;", item?.type, key);
       if (item?.type === "object" || item?.type === "array") {
         const type = item.type;
         const arraySuffix = type === "array" ? "[]" : "";
-        const value =
-          Object.keys(item.object).length > 0
-            ? `{ 
-          ${toTypescript(item.object, options)}
-        }`
-            : "any";
+        const value = Object.keys(item.object || {}).length > 0 ? `{ ${toTypescript(item.object, options)} }` : "any";
         result += `
           ${annotation}
           ${key}: ${value}${arraySuffix};`;
       } else {
         let type = isUndefinedStr(item.type) ? "any" : item.type;
-        if (item?.type?.indexOf("[]") > -1) type += "[]";
+        if (item?.type?.includes("[]")) type += "[]";
         result += `
           ${annotation}
           ${key}: ${type || "string"};`;
@@ -96,5 +89,5 @@ export function toTypescript(data: any, options?: { key?: string; value?: string
 }
 
 function isUndefinedStr(str: string) {
-  return str.indexOf("undefined") > -1;
+  return str?.includes("undefined");
 }
